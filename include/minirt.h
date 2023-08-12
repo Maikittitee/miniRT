fd77c5a65652cfb4860eb64ecc6c2fdc52b95498
ktunchar <maikittitee1615@gmail.com>
2023-08-13 03:53:41 +0700

    docs : add mlx and Makefile

--- include/minirt.h
+++ include/minirt.h
@@ -6,7 +6,16 @@
 /*   By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/13 03:41:23 by ktunchar          #+#    #+#             */
-/*   Updated: 2023/08/13 03:41:24 by ktunchar         ###   ########.fr       */
+/*   Updated: 2023/08/13 03:47:56 by ktunchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MINIRT_H
+# define MINIRT_H
+
+# include "../lib/libft/libft.h"
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+#endif
