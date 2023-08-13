121642cd42d403aa03574f82a8e05d6aa323dc03
Krittithee Tuncharoen <maikittitee1615@gmail.com>
2023-08-14 02:58:30 +0700

    feat: create struct

--- include/minirt.h
+++ include/minirt.h
@@ -6,7 +6,7 @@
 /*   By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/13 03:41:23 by ktunchar          #+#    #+#             */
-/*   Updated: 2023/08/13 03:54:13 by ktunchar         ###   ########.fr       */
+/*   Updated: 2023/08/14 02:48:56 by ktunchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,68 @@
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
+
+typedef struct s_color{
+	unsigned char r;
+	unsigned char g;
+	unsigned char b;
+} t_color;
+
+typedef struct s_pnt{
+	float	x;
+	float	y;
+	float	z;
+} t_pnt;
+
+typedef struct s_vec{
+	float	i;
+	float	j;
+	float	k;
+} t_vec;
+
+typedef struct s_amb{
+	float	ratio;
+	t_color	color;
+} t_amb;
+
+typedef struct s_cam{
+	int 	fov;
+	t_pnt	pnt;
+	t_vec	normal_vec;		
+} t_cam;
+
+typedef struct s_light{
+	float ratio;
+	t_pnt pnt;
+} t_light;
+
+typedef struct s_sphere{
+	t_color color;
+	t_pnt	pnt;
+	float	d;	
+} t_sphere;
+
+typedef	struct s_plane{
+	t_color color;
+	t_pnt	pnt;
+	t_vec	normal_vec;
+} t_plane;
+
+typedef struct s_cylin{
+	t_color	color;
+	t_pnt	pnt;
+	t_vec	normal_vec;
+	float	d;
+	float	h;
+}t_cylin;
+
+typedef struct s_data{
+	t_light		**light;
+	t_sphere	**sphere;
+	t_plane		**plane;
+	t_cylin		**cylin;
+	t_cam		cam;
+	t_amb		amb;
+} t_data;
 
 #endif
