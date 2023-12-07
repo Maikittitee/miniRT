443aa2522664abf2b93a543f9bf11bf2fdc8116c
Krittithee Tuncharoen <maikittitee1615@gmail.com>
2023-12-07 18:17:21 +0700

    stupid gredient

--- src/main.c
+++ src/main.c
@@ -5,54 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/13 03:55:15 by ktunchar          #+#    #+#             */
-/*   Updated: 2023/09/11 18:38:34 by ktunchar         ###   ########.fr       */
+/*   Created: 2023/12/07 16:26:07 by ktunchar          #+#    #+#             */
+/*   Updated: 2023/12/07 16:44:45 by ktunchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minirt.h"
 
-void	mock_data(t_data *data)
+int main()
 {
-	data->amb.color = (t_color){255, 255, 255};
-	data->amb.ratio = 0.2;
-	data->cam.pnt = (t_pnt){0,0,0};
-	data->cam.fov = 70;
-	data->cam.normal_vec = (t_vec){1, 0, 0};
-	data->light = malloc(sizeof(t_light *) * 2);
-	data->light[0] = malloc(sizeof(t_light));
-	data->light[0]->pnt = (t_pnt){20, 20, 0};
-	data->light[0]->ratio = 0.5;
-	data->light[1] = NULL;
-	data->sphere = malloc(sizeof(t_sphere *) * 2);
-	data->sphere[0] = malloc(sizeof(t_sphere));
-	data->sphere[0]->color = (t_color){255, 255, 0};
-	data->sphere[0]->pnt = (t_pnt){20, 0, 0};
-	data->sphere[0]->d = 5;
-	
-}
+    void *mlx = mlx_init();
+    void *win = mlx_new_window(mlx, WIN_WIDTH, WIN_HEIGHT, "Tutorial Window - Create Image");
 
+    void *image = mlx_new_image(mlx, WIN_WIDTH, WIN_HEIGHT);
+    
+	printf("hello\n");
 
-int	main(int ac, char **av)
-{
-	t_data *data;
-	t_img img;
-	char *buffer;
-	// int x;
-	// int y;
-	
-	
-	// protect ac av;
-	if (ac != 2)
-		return (1);
-	data = init_data(*av);
-	mock_data(data);
-	init_camera(&(data->cam));
-	buffer = mlx_get_data_addr(data->img, &(img.pixel_bits), &(img.line_bytes), &(img.endian));
-	ray_tracer(data, buffer, img);
+	render(image, WIN_WIDTH, WIN_HEIGHT);
+    // The following code goes here
 
-	mlx_put_image_to_window(data->mlx, data->win, data->img, 0, 0);
-	mlx_hook(data->win, 17, 0, close_win, data);
-	mlx_hook(data->win, 2, 1, ft_exit, data);
-	mlx_loop(data->mlx);
+	mlx_put_image_to_window(mlx, win, image, 0, 0);
+
+    mlx_loop(mlx);
 }
---
+++ src/renderer.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   renderer.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/07 16:40:14 by ktunchar          #+#    #+#             */
+/*   Updated: 2023/12/07 18:16:47 by ktunchar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minirt.h"
+
+t_bool render(void *img, int width, int height)
+{
+	// int	x;
+	// int	y;
+	
+	int pixel_bits;
+	int line_bytes;
+	int endian;
+	char *buffer = mlx_get_data_addr(img, &pixel_bits, &line_bytes, &endian);
+
+	// x = 0;
+	// y = 0;
+	// while (y < height)
+	// {
+	// 	while (x < width)
+	// 	{
+	// 		buffer[x * 4 + y * line_bytes] = get_color(x, y);
+	// 		x++;
+	// 	}
+	// 	y++;
+
+	// }
+
+// if (pixel_bits != 32)
+//     color = mlx_get_color_value(mlx, color);
+
+for(int y = 0; y < height; ++y)
+for(int x = 0; x < width; ++x)
+{
+	int color = get_color(x, y);
+    int pixel = (y * line_bytes) + (x * 4);
+
+        buffer[pixel + 3] = (color >> 24); // alpha
+        buffer[pixel + 2] = (color >> 16) & 0xFF; //red
+        buffer[pixel + 1] = (color >> 8) & 0xFF; // green
+        buffer[pixel] = (color) & 0xFF; //blue
+}
+	return (True);
+}
+
+int	get_color(int x, int y)
+{
+	(void)x;
+	(void)y;
+
+
+
+	char r = floor(x* 255.0);
+	char g = floor(y * 255.0);
+	char b = 0x00;
+
+	int color = (((char)r << 16) & (0xFF0000)) | (((char)g << 8) & (0x00FF00)) | (b & (0x0000FF));
+
+	printf("%x\n", color);
+	return (color);
+	// return ((r << 16) | (g << 8) | (b));
+	// return (0xABCDEF);	
+	return (0xFF0000);
+	
+}
