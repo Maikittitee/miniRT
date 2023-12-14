e888b74165958025a51776867bbb2378861dd4a6
ktunchar <maikittitee1615@gmail.com>
2023-12-15 03:31:19 +0700

    idk

---
+++ src/math.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   math.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/15 03:10:52 by ktunchar          #+#    #+#             */
+/*   Updated: 2023/12/15 03:19:44 by ktunchar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minirt.h"
+
+t_vec vector_add(t_vec a, t_vec b)
+{
+	t_vec ret;
+
+	ret.i = a.i + b.i;
+	ret.j = a.j + b.j;
+	ret.k = a.k + b.k;
+	
+	return (ret);
+}
+
+t_vec vector_sub(t_vec a, t_vec b)
+{
+	t_vec ret;
+
+	ret.i = a.i - b.i;
+	ret.j = a.j - b.j;
+	ret.k = a.k - b.k;
+	
+	return (ret);
+}
+
+float vector_dot(t_vec a, t_vec b)
+{
+	float result;
+
+	result = a.i * b.i + a.j * b.j + a.k * b.k;
+
+	return (result);
+}
+
+t_vec vector_cross(t_vec a, t_vec b)
+{
+	t_vec ret;
+
+	ret.i = +(a.j * b.k - a.k * b.j);
+	ret.j = -()
+}
+
