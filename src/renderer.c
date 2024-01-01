e580368e0901263adc50801f3be8d7158b0f14da
ktunchar <maikittitee1615@gmail.com>
2024-01-01 21:53:52 +0700

    l

--- src/renderer.c
+++ src/renderer.c
@@ -6,7 +6,7 @@
 /*   By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 16:40:14 by ktunchar          #+#    #+#             */
-/*   Updated: 2023/12/31 04:01:41 by ktunchar         ###   ########.fr       */
+/*   Updated: 2024/01/01 20:47:54 by ktunchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,14 +57,15 @@ t_color	per_pixel(int x, int y)
 
 	// get color from float
 	
-	float aspect_ratio = WIN_WIDTH/WIN_HEIGHT;
+	// float aspect_ratio = WIN_WIDTH/WIN_HEIGHT;
 	
-	i *= aspect_ratio;
-	t_vec origin;
-	origin.i = 0;
-	origin.j = 0;
-	origin.k = -10.0004555555;
-	// origin = vector_norm(origin);
+	// i *= aspect_ratio;
+	t_vec camara;
+	camara.i = 0;
+	// camara.j = 0;
+	// camara.k = -10.0004555555;
+	camara.j = -20;
+	camara.k = -10.0004555555;
 	
 	t_vec direction;
 	direction.i = i;
@@ -73,53 +74,51 @@ t_color	per_pixel(int x, int y)
 	direction = vector_norm(direction);
 
 	float a =  vector_dot(direction, direction);
-	float b = 2 * vector_dot(origin, direction);
-	float c = vector_dot(origin, origin) - 100;
+	float b = 2 * vector_dot(camara, direction);
+	// float c = vector_dot(camara, camara) - 100;
+	float c = vector_dot(camara, camara) - 81;
 
 
+	// printf("direction <%f %f %f>\n", direction.i, direction.j, direction.k);
+	// usleep(100000);
+	// return (t_color){255, 0, 255, 255};
 	float discm; 
-	discm = b * b - 4 * a * c;
+	discm = (b * b) - (4 * a * c);
 
 	if (discm < 0)
 		return ((t_color){0, 0, 0, 255});
-	// return ((t_color){255, 0, 255, 1});
-	// printf("%d\n", (unsigned char)(-0.5 * 255));
-	// return ((t_color){-0.5 * 255, 0, 0, 255});
+	
+	
 	float t2;
-
-	// t1 = ((-1 * b) + sqrt(discm)) / (2 * a);
 	t2 = ((-1 * b) - sqrt(discm)) / (2 * a);
 
-	t_vec hit_point = vector_add(origin, c_vec(t2, direction));
-	// printf("hit point is (%f %f %f)\n", hit_point.i, hit_point.j, hit_point.k);
-
-	// go to light
-
-	t_light light;
-
-	light.ori =  (t_vec){0, 0, -100};
-	light.ratio = 1;
+	t_vec hit_point = vector_add(camara, c_vec(t2, direction));
 	
-	t_vec hp_to_light, normal;
-
-	t_vec circle_origin;
-
-	circle_origin.i = 0;
-	circle_origin.j = 0;
-	circle_origin.k = 0;
-	normal = vector_norm(vector_sub(hit_point, circle_origin));
-	hp_to_light = vector_norm(vector_sub(light.ori, hit_point));
-
-	float dot_p;
-	// dot_p = fmaxf(vector_dot(o_to_hp, hp_to_light), 0.0f);
-	dot_p = fmaxf(vector_dot(normal, hp_to_light), 0.0f);
-	// if (dot_p < 0)
-	// 	return ((t_color){0, 0, 0, 255});
+	printf("hp (%f, %f, %f)\n", hit_point.i, hit_point.j, hit_point.k);
+	// t_light light;
+	// light.ori =  (t_vec){-50, -50, -50};
+	// light.ratio = 1;
 	
-	printf("hp(%f, %f, %f) normal vector<%f %f %f> ray_to_light<%f %f %f>:::%f\n", hit_point.i, hit_point.j, hit_point.k, normal.i, normal.j, normal.k, hp_to_light.i, hp_to_light.j, hp_to_light.k, dot_p);
-	// printf("(%d, %d)dot product result:%f, color:%f\n",x, y, dot_p, dot_p * 255.0f); 
+	// t_vec hp_to_light, normal;
+	// t_vec circle_origin;
+
+	// circle_origin.i = 0;
+	// circle_origin.j = 0;
+	// circle_origin.k = 0;
+	// normal = vector_norm(vector_sub(circle_origin, hit_point));
+	// hp_to_light = vector_norm(vector_sub(hit_point, light.ori));
+
+	// float dot_p;
+	// // dot_p = fmaxf(vector_dot(o_to_hp, hp_to_light), 0.0f);
+	// dot_p = fmaxf(vector_dot(normal, hp_to_light), 0.0f);
+	// // if (dot_p < 0)
+	// // 	return ((t_color){0, 0, 0, 255});
+	
+	// printf("hp(%f, %f, %f) normal vector<%f %f %f> ray_to_light<%f %f %f>:::%f\n", hit_point.i, hit_point.j, hit_point.k, normal.i, normal.j, normal.k, hp_to_light.i, hp_to_light.j, hp_to_light.k, dot_p);
+	// // printf("(%d, %d)dot product result:%f, color:%f\n",x, y, dot_p, dot_p * 255.0f); 
 
 	
-	return ((t_color){(dot_p) * 255, 0, 0 , 255});
+	return ((t_color){255, 0, 0, 255});
+	// return ((t_color){(dot_p) * 255, 0, 0 , 255});
 
 }
