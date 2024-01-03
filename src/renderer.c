fd6a9a503f196d5025be8bad224a00581f68f365
ktunchar <maikittitee1615@gmail.com>
2024-01-03 15:48:24 +0700

    idk shit

--- src/renderer.c
+++ src/renderer.c
@@ -6,7 +6,7 @@
 /*   By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 16:40:14 by ktunchar          #+#    #+#             */
-/*   Updated: 2024/01/01 20:47:54 by ktunchar         ###   ########.fr       */
+/*   Updated: 2024/01/02 22:42:13 by ktunchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,22 +64,30 @@ t_color	per_pixel(int x, int y)
 	camara.i = 0;
 	// camara.j = 0;
 	// camara.k = -10.0004555555;
-	camara.j = -20;
-	camara.k = -10.0004555555;
+	camara.j = 0;
+	camara.k = -20;
+	// camara = vector_norm(camara);
 	
 	t_vec direction;
 	direction.i = i;
 	direction.j = j;
 	direction.k = 1;
+	 if (i == 0 && j == -10)
+	{
+	printf("direction <%f %f %f>\n |direction| = %f\n", direction.i, direction.j, direction.k, vector_size(direction));
+	}
 	direction = vector_norm(direction);
 
 	float a =  vector_dot(direction, direction);
-	float b = 2 * vector_dot(camara, direction);
-	// float c = vector_dot(camara, camara) - 100;
-	float c = vector_dot(camara, camara) - 81;
+	float b = 2 * vector_dot(direction, vector_norm(camara));
+	float c = vector_dot(vector_norm(camara), vector_norm(camara)) - 100;
+	// float c = vector_dot(camara, camara) - 81;
 
-
-	// printf("direction <%f %f %f>\n", direction.i, direction.j, direction.k);
+	if (i == 0 && j == -10)
+	{
+	printf("normed direction <%f %f %f>\n|direction| = %f\n", direction.i, direction.j, direction.k, vector_size(direction));
+		printf("%ft^2 + %ft + %f = 0\n", a, b, c);
+	}
 	// usleep(100000);
 	// return (t_color){255, 0, 255, 255};
 	float discm; 
@@ -91,34 +99,43 @@ t_color	per_pixel(int x, int y)
 	
 	float t2;
 	t2 = ((-1 * b) - sqrt(discm)) / (2 * a);
+	float t1;
+	t1 = ((-1 * b) + sqrt(discm)) / (2 * a);
 
-	t_vec hit_point = vector_add(camara, c_vec(t2, direction));
+	t2 = fminf(t1,  t2);
+	t_vec cam_to_hp = c_vec(t2, direction);
+	t_vec hit_point = vector_add(camara, cam_to_hp);
 	
-	printf("hp (%f, %f, %f)\n", hit_point.i, hit_point.j, hit_point.k);
-	// t_light light;
-	// light.ori =  (t_vec){-50, -50, -50};
-	// light.ratio = 1;
+	if (i == 0 && (j == -10 || j == 0))
+	{
+		printf("t: %f", t2);
+		printf("cam to hp: %f %f %f", cam_to_hp.i, cam_to_hp.j, cam_to_hp.k);
+		printf("hp (%f, %f, %f)\n", hit_point.i, hit_point.j, hit_point.k);
+	}
+	t_light light;
+	light.ori =  (t_vec){-50, -50, -50};
+	light.ratio = 1;
 	
-	// t_vec hp_to_light, normal;
-	// t_vec circle_origin;
+	t_vec hp_to_light, normal;
+	t_vec circle_origin;
 
-	// circle_origin.i = 0;
-	// circle_origin.j = 0;
-	// circle_origin.k = 0;
-	// normal = vector_norm(vector_sub(circle_origin, hit_point));
-	// hp_to_light = vector_norm(vector_sub(hit_point, light.ori));
+	circle_origin.i = 0;
+	circle_origin.j = 0;
+	circle_origin.k = 0;
+	normal = vector_norm(vector_sub(circle_origin, hit_point));
+	hp_to_light = vector_norm(vector_sub(hit_point, light.ori));
 
-	// float dot_p;
-	// // dot_p = fmaxf(vector_dot(o_to_hp, hp_to_light), 0.0f);
-	// dot_p = fmaxf(vector_dot(normal, hp_to_light), 0.0f);
-	// // if (dot_p < 0)
-	// // 	return ((t_color){0, 0, 0, 255});
+	float dot_p;
+	// dot_p = fmaxf(vector_dot(o_to_hp, hp_to_light), 0.0f);
+	dot_p = fmaxf(vector_dot(normal, hp_to_light), 0.0f);
+	// if (dot_p < 0)
+	// 	return ((t_color){0, 0, 0, 255});
 	
-	// printf("hp(%f, %f, %f) normal vector<%f %f %f> ray_to_light<%f %f %f>:::%f\n", hit_point.i, hit_point.j, hit_point.k, normal.i, normal.j, normal.k, hp_to_light.i, hp_to_light.j, hp_to_light.k, dot_p);
-	// // printf("(%d, %d)dot product result:%f, color:%f\n",x, y, dot_p, dot_p * 255.0f); 
+	printf("hp(%f, %f, %f) normal vector<%f %f %f> ray_to_light<%f %f %f>:::%f\n", hit_point.i, hit_point.j, hit_point.k, normal.i, normal.j, normal.k, hp_to_light.i, hp_to_light.j, hp_to_light.k, dot_p);
+	// printf("(%d, %d)dot product result:%f, color:%f\n",x, y, dot_p, dot_p * 255.0f); 
 
 	
-	return ((t_color){255, 0, 0, 255});
-	// return ((t_color){(dot_p) * 255, 0, 0 , 255});
+	// return ((t_color){255, 0, 0, 255});
+	return ((t_color){(dot_p) * 255, 0, 0 , 255});
 
 }
