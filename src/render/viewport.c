4f72db6dc3606dccf5caa3f2bbb5bc99a4c0d380
ktunchar <maikittitee1615@gmail.com>
2024-01-30 18:46:54 +0700

    fix: readable

--- src/render/viewport.c
+++ src/render/viewport.c
@@ -6,7 +6,7 @@
 /*   By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 23:23:13 by ktunchar          #+#    #+#             */
-/*   Updated: 2024/01/29 01:21:16 by ktunchar         ###   ########.fr       */
+/*   Updated: 2024/01/30 13:03:33 by ktunchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,8 @@ t_axis calculate_axis(t_vec dir) // dir should be normed
 t_bool	init_viewport(t_viewport *viewport, t_cam *cam)
 {
 	t_vec first_pixel;
-
+	t_vec viewport_u;
+	t_vec viewport_v;
 	float aspect_ratio;
 
 	aspect_ratio = (float)WIN_HEIGHT/ (float)WIN_WIDTH;
@@ -39,13 +40,29 @@ t_bool	init_viewport(t_viewport *viewport, t_cam *cam)
 	viewport->origin = cam->origin;
 	viewport->width = 2.0f * tan(cam->fov * 0.0174532925f);
 	viewport->height = aspect_ratio * viewport->width;
-	viewport->axis = calculate_axis(vector_norm(cam->direction));
-	viewport->width_vec = vector_scaler(viewport->width, viewport->axis.x);
-	viewport->height_vec = vector_scaler(viewport->height, viewport->axis.y);
-	viewport->step_x = vector_scaler((float)(1 / (float)WIN_WIDTH) ,viewport->width_vec);
-	viewport->step_y = vector_scaler((float)(1 / (float)WIN_HEIGHT) ,viewport->height_vec);
-	first_pixel = vector_sub(viewport->origin, vector_scaler(0.5f, viewport->width_vec));
-	first_pixel = vector_sub(first_pixel, vector_scaler(0.5f, viewport->height_vec));
+
+
+
+	viewport_u = (t_vec){viewport->width, 0 , 0};
+	viewport_v = (t_vec){0 ,-1 * viewport->height, 0};
+
+	viewport->width_vec = viewport_u;	
+	viewport->height_vec = viewport_v;	
+	viewport->step_x = vector_scaler(1.0f/ viewport->width , viewport_u);
+	viewport->step_y = vector_scaler(1.0f/ viewport->height , viewport_v);
+	
+	first_pixel = vector_add(cam->origin, (t_vec){0, 0, -1});
+	first_pixel = vector_add(first_pixel, vector_scaler(-0.5f, viewport_u));
+	first_pixel = vector_add(first_pixel, vector_scaler(-0.5f, viewport_v));
+	
+	
+	viewport->axis = calculate_axis(vector_norm(cam->direction)); // x is right , y is up, -z is direction
+	// viewport->width_vec = vector_scaler(viewport->width, viewport->axis.x);
+	// viewport->height_vec = vector_scaler(viewport->height, viewport->axis.y);
+	// viewport->step_x = vector_scaler((float)(1 / (float)WIN_WIDTH) ,viewport->width_vec);
+	// viewport->step_y = vector_scaler((float)(1 / (float)WIN_HEIGHT) ,viewport->height_vec);
+	// first_pixel = vector_sub(viewport->origin, vector_scaler(0.5f, viewport->width_vec));
+	// first_pixel = vector_sub(first_pixel, vector_scaler(0.5f, viewport->height_vec));
 	viewport->upper_left_px = first_pixel;
 	return (True);
 }
