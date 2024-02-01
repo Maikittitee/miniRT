3d9a349e6283b17d447572aab67a7a4885f50207
Krittithee Tuncharoen <maikittitee1615@gmail.com>
2024-02-02 02:23:40 +0700

    pass

--- src/render/main.c
+++ src/render/main.c
@@ -6,7 +6,7 @@
 /*   By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 16:26:07 by ktunchar          #+#    #+#             */
-/*   Updated: 2024/02/01 00:35:29 by ktunchar         ###   ########.fr       */
+/*   Updated: 2024/02/02 02:23:12 by ktunchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,58 +14,16 @@
 
 int	main()
 {
-	t_viewport viewport;
-	t_data	data;
+	t_vec u;
+	t_vec v;
 
+	u = (t_vec){1, 0, 0};
+	v = (t_vec){0,1,0};
 
-	t_cam	cam;
+	print_vec(u);
+	print_vec(v);
 
-	cam.direction = (t_vec){0, 1, 0};
-	cam.fov = 70;
-	cam.origin = (t_vec){0, 0, 0};
-
-	init_viewport(&viewport, &cam);
-
-	printf("virtual viewport: %fx%f\n", viewport.height, viewport.width);
-	printf("origin\n");
-	print_vec(viewport.origin);
-	printf("width vec / height vec\n");
-	print_vec(viewport.width_vec);
-	print_vec(viewport.height_vec);
-	printf("step x,y\n");
-	print_vec(viewport.step_x);
-	print_vec(viewport.step_y);
-	printf("first pixel\n");
-	print_vec(viewport.upper_left_px);
-	// printf("x y z\n");
-	// print_vec(viewport.axis.x);
-	// print_vec(viewport.axis.y);
-	// print_vec(viewport.axis.z);
-
-	data.viewport = viewport; // check : can assign 
-
-
-	void *mlx;
-	t_img img;
-	char *buffer;
-
-	mlx = mlx_init();
-	data.winp = mlx_new_window(mlx, WIN_WIDTH, WIN_HEIGHT, "pmew");
-    data.imgp = mlx_new_image(mlx, WIN_WIDTH, WIN_HEIGHT);
-	buffer = mlx_get_data_addr(data.imgp, &img.pixel_bits, &img.line_bytes, &img.endian);
-	
-	data.nobj = 1;
-	data.obj = malloc(sizeof(t_obj) * data.nobj);
-	data.obj[0].type = SPHERE;
-	data.obj[0].color = (t_color){255, 0, 0, 255};
-	data.obj[0].ori = (t_vec){0, 0, -10};
-	data.obj[0].d = 15;
-	
-	// rendering
-	render(&data, &img, &buffer);
-	mlx_put_image_to_window(mlx, data.winp, data.imgp, 0, 0);
-
-	// loop
-    mlx_loop(mlx);	
+	t_vec g = vector_dot(u, v);
+	print_vec(g);
 }
 
