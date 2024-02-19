09ccec8aee9ae059ea17642347fd7cda97372f19
Krittithee Tuncharoen <maikittitee1615@gmail.com>
2024-02-19 16:38:46 +0700

    clean debug msg

--- src/render/plane.c
+++ src/render/plane.c
@@ -6,7 +6,7 @@
 /*   By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 22:11:43 by ktunchar          #+#    #+#             */
-/*   Updated: 2024/02/18 17:47:54 by ktunchar         ###   ########.fr       */
+/*   Updated: 2024/02/19 16:38:33 by ktunchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,13 @@
 
 float hit_plane(t_ray ray, t_obj plane)
 {
-	// check is ray dirrection (associate to ray origin) are possible to point to plane
 	float denom = vector_dot(vector_norm(plane.normal_vec), vector_norm(ray.dir)); // know that is a front or back of plane
-	// float denom = fabsf(vector_dot(vector_norm(plane.normal_vec), vector_norm(ray.dir))); // know that is a front or back of plane
-	// printf("denom: %f\n", denom);
 	if (fabsf(denom) < VERYSMALLNUMBER)
 		return (-1.0f); // no plane or back of plane
 	float t;
 	t = vector_dot(vector_sub(plane.ori, ray.ori), vector_norm(plane.normal_vec)) / denom; 
-	printf("calulated t: %f\n", t);
 	if (t <= 0.0f)
 	{
-		printf("and it less than zero\n");
 		return (-1.0f);
 	}
 	return (t);	
