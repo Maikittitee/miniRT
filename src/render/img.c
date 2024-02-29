845a5b1f5b46aba83f857511940d3d523a6c7f8e
Krittithee Tuncharoen <maikittitee1615@gmail.com>
2024-02-29 17:27:50 +0700

    norm img.c

--- src/render/img.c
+++ src/render/img.c
@@ -6,7 +6,7 @@
 /*   By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 01:28:07 by ktunchar          #+#    #+#             */
-/*   Updated: 2024/01/29 01:07:00 by ktunchar         ###   ########.fr       */
+/*   Updated: 2024/02/29 17:27:26 by ktunchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,17 @@
 
 void	my_put_to_img(char *buffer, t_img img, t_vec pnt, t_color color)
 {
-	int pixel = (pnt.y * img.line_bytes) + (pnt.x * 4);
-	// printf("color: %d %d %d\n", color.r, color.g, color.b);
-	if (img.endian == 1)        // Most significant (Alpha) byte first
+	int	pixel;
+
+	pixel = (pnt.y * img.line_bytes) + (pnt.x * 4);
+	if (img.endian == 1)
 	{
 		buffer[pixel + 0] = (1);
 		buffer[pixel + 1] = (color.r) & 0xFF;
 		buffer[pixel + 2] = (color.g) & 0xFF;
 		buffer[pixel + 3] = (color.b) & 0xFF;
 	}
-	else if (img.endian == 0)   // Least significant (Blue) byte first
+	else if (img.endian == 0)
 	{
 		buffer[pixel + 0] = (color.b) & 0xFF;
 		buffer[pixel + 1] = (color.g) & 0xFF;
