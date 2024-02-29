01775628155fc3186b5029ca99e466750e0872d6
Krittithee Tuncharoen <maikittitee1615@gmail.com>
2024-02-29 17:39:12 +0700

    np norm plane.c

--- src/render/plane.c
+++ src/render/plane.c
@@ -6,22 +6,22 @@
 /*   By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 22:11:43 by ktunchar          #+#    #+#             */
-/*   Updated: 2024/02/19 16:38:33 by ktunchar         ###   ########.fr       */
+/*   Updated: 2024/02/29 17:38:56 by ktunchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minirt.h"
 
-float hit_plane(t_ray ray, t_obj plane)
+float	hit_plane(t_ray ray, t_obj plane)
 {
-	float denom = vector_dot(vector_norm(plane.normal_vec), vector_norm(ray.dir)); // know that is a front or back of plane
+	float	denom;
+	float	t;
+
+	denom = vector_dot(vector_norm(plane.normal_vec), vector_norm(ray.dir));
 	if (fabsf(denom) < VERYSMALLNUMBER)
-		return (-1.0f); // no plane or back of plane
-	float t;
+		return (-1.0f);
 	t = vector_dot(vector_sub(plane.ori, ray.ori), vector_norm(plane.normal_vec)) / denom; 
 	if (t <= 0.0f)
-	{
 		return (-1.0f);
-	}
-	return (t);	
+	return (t);
 }
