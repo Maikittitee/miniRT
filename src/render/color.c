221a27e90f24432faa992a5eb69f7ab2f58043e3
Krittithee Tuncharoen <maikittitee1615@gmail.com>
2024-02-29 17:19:39 +0700

    norm color.c

--- src/render/color.c
+++ src/render/color.c
@@ -6,7 +6,7 @@
 /*   By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 00:41:21 by ktunchar          #+#    #+#             */
-/*   Updated: 2024/02/28 20:25:50 by ktunchar         ###   ########.fr       */
+/*   Updated: 2024/02/29 17:19:19 by ktunchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,26 @@
 
 t_color	color_scaler(float c, t_color color)
 {
-	return ((t_color){color.r * c, color.g * c, color.b * c, color.a}) ;
+	return ((t_color){color.r * c, color.g * c, color.b * c, color.a});
 }
 
 t_color	color_add(t_color c1, t_color c2)
 {
-	float r = 0.5f * c1.r + 0.5f * c2.r;
-	float g = 0.5f * c1.g + 0.5f * c2.g;
-	float b = 0.5f * c1.b + 0.5f * c2.b;
+	float	r;
+	float	g;
+	float	b;
 
+	r = 0.5f * c1.r + 0.5f * c2.r;
+	g = 0.5f * c1.g + 0.5f * c2.g;
+	b = 0.5f * c1.b + 0.5f * c2.b;
 	return ((t_color){fmin(r, 255), fmin(g, 255), fmin(b, 255), 255});
 }
 
-t_color calculate_color(t_hit hit, t_ray ray, t_data data)
+t_color	calculate_color(t_hit hit, t_ray ray, t_data data)
 {
-	ray.dir = vector_norm(ray.dir);
+	float	dot;
 
-	float dot = get_dot_product(data.obj[hit.index], data.light, hit, data, ray);
-	// dot = 1;
-	// printf("dot: %f\n", dot);
+	ray.dir = vector_norm(ray.dir);
+	dot = get_dot_product(data.obj[hit.index], data.light, hit, data, ray);
 	return (color_scaler(dot * data.light.ratio, data.obj[hit.index].color));
-	
 }
